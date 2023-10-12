3535980e49c0be6edb45c00c64ee6377ca516642
assis04 <143444812+assis04@users.noreply.github.com>
2023-10-12 12:10:35 -0300

    Adicionei o JS
    Adicionei o JS para interatividade do site

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+const botoes = document.querySelectorAll('.botao');
+const personagem = document.querySelectorAll('.personagem');
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagem[indice].classList.add('selecionado');
+    });
+});
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove('selecionado');
+};
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+};
+
